48c7daf7f80c224598aadb1a54e4d41525eb5ba8
Neto Accioly <lindemiraccioly@gmail.com>
2025-07-28 16:56:44 -0300

    Adicionando o hook de beforeEach para captura do token

--- test/transferencia.test.js
+++ test/transferencia.test.js
@@ -1,22 +1,26 @@
 const request = require('supertest');
 const { expect } = require('chai');
 require('dotenv').config()
-const { obterToken } = require ('../helpers/autenticacao')
+const { obterToken } = require ('../helpers/autenticacao.js')
 
 describe('Transferencias', ()=>{
     describe('Post /transferencias', ()=>{
+        let token
+
+        beforeEach( async() => {
+            token = await obterToken('julio.lima', '123456')
+        })
+
         it('Deve retornar sucesso com 201 quando o valor da transferência for igual ou acima de R$ 10,00', async() => {
-            const token = await obterToken('julio.lima', '123456')
-            
             const resposta = await request(process.env.BASE_URL)
                 .post('/transferencias')
                 .set('Content-Type', 'application/json')
                 .set('Authorization', `Bearer ${token}`)
                 .send({
-                    'contaOrigem': 1,
-                    'contaDestino': 2,
-                    'valor': 11,
-                    'token': ""
+                    contaOrigem: 1,
+                    contaDestino: 2,
+                    valor: 11,
+                    token: ""
                     })
 
                     expect(resposta.status).to.equal(201);
@@ -24,17 +28,15 @@ describe('Transferencias', ()=>{
         })
 
         it('Deve retornar falha com 422 quando o valor da transferência for abaixo de R$ 10,00', async() => {
-            const token = await obterToken('julio.lima', '123456')
-            
             const resposta = await request(process.env.BASE_URL)
                 .post('/transferencias')
                 .set('Content-Type', 'application/json')
                 .set('Authorization', `Bearer ${token}`)
                 .send({
-                    'contaOrigem': 1,
-                    'contaDestino': 2,
-                    'valor': 7,
-                    'token': ""
+                    contaOrigem: 1,
+                    contaDestino: 2,
+                    valor: 7,
+                    token: ""
                     })
 
                     expect(resposta.status).to.equal(422);
